165a21df7010a3cf637588be0864c65de1929e21
jyoti-yb <balaboinajyotiyadav@gmail.com>
2025-08-20 09:38:39 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/controller/ArithematicController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin("*")
+public class ArithematicController {
+	@GetMapping("/add/{A}/{B}")
+	public String add(@PathVariable("A") int a, @PathVariable("B") int b)
+	{
+		return "Addition = " + (a+b);
+	}
+	@GetMapping("/sub/{A}/{B}")
+	public String sub(@PathVariable("A") int a, @PathVariable("B") int b)
+	{
+		return "Sub = " + (a-b);
+	}
+	
+}
